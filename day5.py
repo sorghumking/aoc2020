f026d3b147f3c3f23ce65d8e7aa04ed5449c3a40
Brian G <sorghumking@yahoo.com>
2020-12-05 19:03:42 -0600

    Day 5 comments, one-liner for fun

--- day5.py
+++ day5.py
@@ -2,10 +2,13 @@ def parse_input():
     with open("inputs/day5.txt") as f:
         return [l.strip() for l in f.readlines()]
 
+# Boarding pass rows are are just binary in disguise.
+# Map F -> 0 and B -> 1 and convert the string to decimal.
 def get_row(p):
     binary = p.replace('F', '0').replace('B', '1')
     return int(binary, 2)
 
+# Same goes for seats: L -> 0 and R -> 1.
 def get_seat(p):
     binary = p.replace('L', '0').replace('R', '1')
     return int(binary, 2)
@@ -18,6 +21,9 @@ def part1(passes):
         if cur_id > max_id:
             max_id = cur_id
     print(f"Max seat ID is {max_id}.")
+
+def part1_oneliner(passes):
+    print(f"Max seat ID is {max([get_row(p[:7]) * 8 + get_seat(p[7:]) for p in passes])}.")
 
 def part2(passes):
     ids = sorted([get_row(p[:7]) * 8 + get_seat(p[7:]) for p in passes])
