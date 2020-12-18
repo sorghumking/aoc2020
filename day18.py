c080bbcfa8cfbcd14495e9f4461cacb61340c4fa
Brian G <sorghumking@yahoo.com>
2020-12-18 17:42:32 -0600

    Day 18 refactored into single evaluate() f'n

--- day18.py
+++ day18.py
@@ -34,52 +34,32 @@ def parse_input(input_file):
                 break
     return probs
 
-
-def part1(probs):
-    results = [evaluate(p) for p in probs]
-    print(f"Sum of all results is {sum(results)}")
-
-def evaluate(exp):
+def evaluate(exp, precedence):
     op = None
-    vals = []
-    for idx, c in enumerate(exp):
-        if type(c) is list:
-            vals.append(evaluate(c))
-        elif type(c) is int:
-            vals.append(c)
-        else:
-            op = c
-
-        if len(vals) == 2: 
-            result = compute(op, vals[0], vals[1])
-            vals = [result]
-    return vals[0]
-
-def evaluate2(exp):
-    op = None
-    vals = []
-    new_exp = []
-    for idx, c in enumerate(exp):
-        if type(c) is list:
-            vals.append(evaluate2(c))
-        elif type(c) is int:
-            vals.append(c)
-        else:
-            op = c
-
-        if len(vals) == 2: 
-            if op == '*':
-                new_exp.append(vals[0])
-                new_exp.append('*')
-                vals = [vals[1]]
+    cur_exp = exp
+    for cur_op in precedence if precedence is not None else [0]: # loop once if no precedence
+        vals = []
+        new_exp = []
+        for idx, c in enumerate(cur_exp):
+            if type(c) is list:
+                vals.append(evaluate(c, precedence))
+            elif type(c) is int:
+                vals.append(c)
             else:
-                result = compute(op, vals[0], vals[1])
-                vals = [result]
-    if len(new_exp) == 0:
-        return vals[0]
-    else:
-        return evaluate(new_exp + [vals[0]])
+                op = c
 
+            if len(vals) == 2: 
+                if precedence is not None and op != cur_op:
+                    # op has lower preference than current, stash values
+                    # and proceed; op will be computed on a future loop.
+                    new_exp.append(vals[0])
+                    new_exp.append(op)
+                    vals = [vals[1]]
+                else:
+                    result = compute(op, vals[0], vals[1])
+                    vals = [result]
+        cur_exp = new_exp + [vals[0]]
+    return vals[0]
 
 def compute(op, val1, val2):
     if op == '+':
@@ -87,8 +67,12 @@ def compute(op, val1, val2):
     elif op == '*':
         return val1 * val2
 
+def part1(probs):
+    results = [evaluate(p, None) for p in probs]
+    print(f"Sum of all results is {sum(results)}")
+
 def part2(probs):
-    results = [evaluate2(p) for p in probs]
+    results = [evaluate(p, ['+', '*']) for p in probs]
     print(f"Sum of all results is {sum(results)}")
 
 if __name__ == "__main__":
